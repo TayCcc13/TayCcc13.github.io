a5d87f869ac99b3c43d8d2586ccda508031c64c6
TayCcc13 <471692885@qq.com>
2024-01-25 20:01:54 +0800

    Site updated: 2024-01-25 20:01:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/28/嵌入式/Makefile/【嵌入式】Makefile/","2023/06/03/嵌入式/NFS/【嵌入式】开发板挂载Ubuntu的NFS目录/","2023/05/22/嵌入式/环境搭建/【嵌入式】虚拟机Ubuntu环境配置/","2023/08/13/嵌入式/文件IO/【嵌入式】文件IO/","2023/10/15/嵌入式/第一个驱动/【嵌入式】第一个驱动/"];function toRandomPost(){
+var posts=["2023/05/28/嵌入式/Makefile/【嵌入式】Makefile/","2023/06/03/嵌入式/NFS/【嵌入式】开发板挂载Ubuntu的NFS目录/","2023/08/13/嵌入式/文件IO/【嵌入式】文件IO/","2023/05/22/嵌入式/环境搭建/【嵌入式】虚拟机Ubuntu环境配置/","2023/10/15/嵌入式/第一个驱动/【嵌入式】第一个驱动/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
