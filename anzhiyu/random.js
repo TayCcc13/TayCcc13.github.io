ec45c2a214f98bfe0ffe50ca327e105b07eee563
TayCcc13 <471692885@qq.com>
2024-03-08 16:17:21 +0800

    Site updated: 2024-03-08 16:17:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/07/LeetCode/【LeetCode】移除元素Day2/","2024/03/07/LeetCode/【LeetCode】删除有序数组中的重复项Day3/","2023/05/28/嵌入式/Makefile/【嵌入式】Makefile/","2023/06/03/嵌入式/NFS/【嵌入式】开发板挂载Ubuntu的NFS目录/","2023/08/13/嵌入式/文件IO/【嵌入式】文件IO/","2023/05/22/嵌入式/环境搭建/【嵌入式】虚拟机Ubuntu环境配置/","2023/10/15/嵌入式/第一个驱动/【嵌入式】第一个驱动/"];function toRandomPost(){
+var posts=["2024/03/08/LeetCode/【LeetCode】删除有序数组中的重复项Day3/","2024/03/08/LeetCode/【LeetCode】删除有序数组中的重复项ⅡDay4/","2024/03/07/LeetCode/【LeetCode】移除元素Day2/","2023/05/28/嵌入式/Makefile/【嵌入式】Makefile/","2023/06/03/嵌入式/NFS/【嵌入式】开发板挂载Ubuntu的NFS目录/","2023/05/22/嵌入式/环境搭建/【嵌入式】虚拟机Ubuntu环境配置/","2023/10/15/嵌入式/第一个驱动/【嵌入式】第一个驱动/","2023/08/13/嵌入式/文件IO/【嵌入式】文件IO/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
